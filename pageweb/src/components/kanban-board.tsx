f79456e9df6ed7a8e53b5a0e856409fb0ac316c4
devCaiodias <caiodiasmartins12@gmail.com>
2025-10-08 14:06:27 -0300

    feat: add Kanban components for Code Review, In Progress, Completed, and To Do sections

--- pageweb/src/components/kanban-board.tsx
+++ pageweb/src/components/kanban-board.tsx
@@ -1,3 +1,7 @@
+import { CodeReviewKanban } from "./code-review-kanban";
+import { InProgressKanban } from "./in-progress-kanban";
+import { ToDoKanban } from "./to-do-kanban";
+
 export default function KanbanBoard() {
   return (
     <main className="mx-11 mt-9 flex flex-col space-y-4">
@@ -7,127 +11,16 @@ export default function KanbanBoard() {
       <section className="flex gap-6 overflow-x-auto p-6">
 
         {/* Coluna To Do */}
-        <div className="flex-1 min-w-[280px] space-y-3">
-          <div className="flex items-center gap-2 mb-4">
-            <h2 className="font-semibold text-amber-50">To do</h2>
-            <button className="text-gray-100 hover:text-amber-50">+</button>
-          </div>
+        <ToDoKanban />
 
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Make a new Post</h3>
-              <p className="text-sm text-gray-500">
-                In a new post you need to tell about our new action...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 10/28/21</span>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Make a new Post</h3>
-              <p className="text-sm text-gray-500">
-                In a new post you need to tell about our new action...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 10/28/21</span>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Make a new Post</h3>
-              <p className="text-sm text-gray-500">
-                In a new post you need to tell about our new action...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 10/28/21</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* Coluna In progress */}
+        <InProgressKanban />
 
-        {/* Coluna Doing */}
-        <div className="flex-1 min-w-[280px] space-y-3">
-          <div className="flex items-center gap-2 mb-4">
-            <h2 className="font-semibold text-amber-50">In progress</h2>
-            <button className="text-gray-100 hover:text-amber-50">+</button>
-          </div>
+        {/* Coluna Code review */}
+        <CodeReviewKanban />
 
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Check design materials</h3>
-              <p className="text-sm text-gray-500">
-                Please have a look at Barone LLC marketing materials...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 5/7/21</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex-1 min-w-[280px] space-y-3">
-          <div className="flex items-center gap-2 mb-4">
-            <h2 className="font-semibold text-amber-50">Code review</h2>
-            <button className="text-gray-100 hover:text-amber-50">+</button>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Check design materials</h3>
-              <p className="text-sm text-gray-500">
-                Please have a look at Barone LLC marketing materials...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 5/7/21</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Check design materials</h3>
-              <p className="text-sm text-gray-500">
-                Please have a look at Barone LLC marketing materials...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 5/7/21</span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex-1 min-w-[280px] space-y-3">
-          <div className="flex items-center gap-2 mb-4">
-            <h2 className="font-semibold text-amber-50">Completed</h2>
-            <button className="text-gray-100 hover:text-amber-50">+</button>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Check design materials</h3>
-              <p className="text-sm text-gray-500">
-                Please have a look at Barone LLC marketing materials...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 5/7/21</span>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="bg-white shadow rounded-xl p-4 flex flex-col gap-2">
-              <h3 className="font-semibold">Check design materials</h3>
-              <p className="text-sm text-gray-500">
-                Please have a look at Barone LLC marketing materials...
-              </p>
-              <div className="flex items-center text-sm text-gray-400 gap-2 mt-2">
-                <span>📅 5/7/21</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* Coluna Completed */}
+        
 
       </section>
     </main>
